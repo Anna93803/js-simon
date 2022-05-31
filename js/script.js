d0d2aea01414173f9897b7f7fd4aae1ea1881c2a
Anna93803 <anna.tassone93@gmail.com>
2022-05-31 16:23:38 +0200

    add timer in progress

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-// Visualizzare in pagina (html) 5 numeri casuali.
-// Avviare un timer di 30 secondi
+
 // Dopo 30 secondi, nascondere i numeri.
 // L’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite un prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+
+
+// Visualizzare in pagina (html) 5 numeri casuali.
 // recupero dall'html l'id per poter stampare a schermo poi i numeri
 const numeriHtml = document.getElementById("numeri");
-
+const timer = document.getElementById("timer");
 // creo un array che continene i 5 numeri generati casualmente
 const numeriCasuali = [];
 
@@ -20,4 +22,24 @@ for(let i = 0; i < 5; i++) {
 }
 console.log(numeriCasuali);
 
-numeriHtml.innerHTML = numeriCasuali;
+numeriHtml.innerHTML = numeriCasuali.join(" - ");
+
+
+// Avviare un timer di 30 secondi
+let secondiRimanenti = 30;
+
+let contoRovescia = setTimeout( function() {
+
+    secondiRimanenti --;
+    timer.innerHTML = "00" + ":" + secondiRimanenti;
+
+    if(secondiRimanenti === 0) {
+        clearInterval(contoRovescia)
+        numeriHtml.classList.add("d-none");
+    }
+
+    console.log(secondiRimanenti);
+
+}, 3000);
+
+// console.log(contoRovescia);
